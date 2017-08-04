9f0fb120aeb317c3b50fecd95b881eaae7890481
Marco Vidal Garcia <marco.vidal@cern.ch>
2017-08-04 10:34:53 +0200

    Test printing env vars

--- indico/run_indico.sh
+++ indico/run_indico.sh
@@ -3,6 +3,9 @@
 . /opt/indico/.venv/bin/activate
 
 export SQLALCHEMY_DATABASE_URI="postgresql://$PGUSER:$PGPASSWORD@$PGHOST/$PGDATABASE"
+echo $PGUSER
+echo $PGDATABASE
+echo $SQLALCHEMY_DATABASE_URI
 
 psql -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
 
