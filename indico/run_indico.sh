763cb51e02476b1568e63bc57f4106f331a28e9f
Marco Vidal Garcia <marco.vidal@cern.ch>
2017-08-09 19:00:54 +0200

    Remove env var prints

--- indico/run_indico.sh
+++ indico/run_indico.sh
@@ -3,9 +3,6 @@
 . /opt/indico/.venv/bin/activate
 
 export SQLALCHEMY_DATABASE_URI="postgresql://$PGUSER:$PGPASSWORD@$PGHOST/$PGDATABASE"
-echo $PGUSER
-echo $PGDATABASE
-echo $SQLALCHEMY_DATABASE_URI
 
 psql -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
 
