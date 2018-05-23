c59a1b518eae718964571f0c6e2330e7bf5acbb4
Michal Kolodziejski <michal.kolodziejski@cern.ch>
2018-05-23 15:39:31 +0200

    Explicitly connect to PGDATABASE

--- indico/run_indico.sh
+++ indico/run_indico.sh
@@ -2,11 +2,11 @@
 
 . /opt/indico/.venv/bin/activate
 
-psql -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
+psql $PGDATABASE -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
 
 until [ $? -eq 0 ]; do
     sleep 1
-    psql -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
+    psql $PGDATABASE -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
 done
 
 psql -c 'SELECT * FROM events.events'
@@ -15,8 +15,8 @@ if [ $? -eq 1 ]; then
     echo 'Preparing DB...'
     if [ $USE_EXTERNAL_DB == 'y' ]; then
         echo 'Using external database...'
-        echo 'CREATE EXTENSION unaccent;' | psql
-        echo 'CREATE EXTENSION pg_trgm;' | psql
+        echo 'CREATE EXTENSION unaccent;' | psql $PGDATABASE
+        echo 'CREATE EXTENSION pg_trgm;' | psql $PGDATABASE
     else
         echo 'Using PostgreSQL container...'
         echo 'CREATE EXTENSION unaccent;' | psql -U postgres
