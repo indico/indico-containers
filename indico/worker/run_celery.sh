bdfd84f1569716d325c6ae5d90bac94a93244c34
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-08-29 17:58:12 +0200

    Add code to make celery wait for webserver

--- indico/worker/run_celery.sh
+++ indico/worker/run_celery.sh
@@ -3,5 +3,11 @@
 . /opt/indico/set_user.sh
 . /opt/indico/.venv/bin/activate
 
+echo 'Waiting for indico-web to be online...'
+while [[ "$(curl -L --max-time 10 -s -o /dev/null -w ''%{http_code}'' 'http://indico-web:59999')" != "200" ]]; do
+    sleep 10;
+    echo 'Waiting...'
+done
+
 echo 'Starting Celery...'
 indico celery worker -B
