d2d1b82324c4507dd5a180a0cc34b645a22493d5
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-08-30 13:58:46 +0200

    Rename Docker images to fit docker hub org

--- build_latest.sh
+++ build_latest.sh
@@ -2,4 +2,4 @@
 
 LATEST_VERSION=$(curl --insecure https://api.github.com/repos/indico/indico/releases/latest | jq '.name[1:]' --raw-output)
 echo Building Indico ${LATEST_VERSION}...
-docker build indico/worker -t indico:latest -t indico:$LATEST_VERSION --build-arg tag=${LATEST_VERSION}
+docker build indico/worker -t getindico/indico:latest -t getindico/indico:$LATEST_VERSION --build-arg tag=${LATEST_VERSION}
