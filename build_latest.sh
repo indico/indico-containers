ea527745b7c2b6da65a05d9aae24c2be301a28ba
Tomas Roun <tomas.roun@cern.ch>
2023-02-28 21:27:22 +0100

    check if jq is installed in build script

--- build_latest.sh
+++ build_latest.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+# Builds the latest Indico image
+
+if ! command -v jq >/dev/null 2>&1
+then
+    echo 'jq is not installed'
+    exit
+fi
 
 LATEST_VERSION=$(curl --insecure https://api.github.com/repos/indico/indico/releases/latest | jq '.name[1:]' --raw-output)
-echo Building Indico ${LATEST_VERSION}...
+echo "Building Indico ${LATEST_VERSION}..."
 docker build indico/worker -t getindico/indico:latest -t getindico/indico:$LATEST_VERSION --build-arg tag=${LATEST_VERSION}
