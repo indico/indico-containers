f34592f82b7682e7b71fc026fae5cc9305a51c59
Tomas Roun <tomas.roun8@gmail.com>
2023-03-01 15:39:58 +0100

    Simplify run_indico.sh

--- indico/worker/run_indico.sh
+++ indico/worker/run_indico.sh
@@ -3,26 +3,25 @@
 /opt/indico/set_user.sh
 . /opt/indico/.venv/bin/activate
 
-psql $PGDATABASE -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
+connect_to_db() {
+    psql -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
+}
 
+# Wait until the DB becomes available
+connect_to_db
 until [ $? -eq 0 ]; do
+    echo "Waiting for DB to become available..."
     sleep 1
-    psql $PGDATABASE -lqt | cut -d \| -f 1 | grep -qw $PGDATABASE
+    connect_to_db
 done
 
+# Check whether the DB is already setup
 psql -c 'SELECT COUNT(*) FROM events.events'
 
 if [ $? -eq 1 ]; then
     echo 'Preparing DB...'
-    if [ "$USE_EXTERNAL_DB" == 'y' ]; then
-        echo 'Using external database...'
-        echo 'CREATE EXTENSION unaccent;' | psql
-        echo 'CREATE EXTENSION pg_trgm;' | psql
-    else
-        echo 'Using PostgreSQL container...'
-        echo 'CREATE EXTENSION unaccent;' | psql -U postgres
-        echo 'CREATE EXTENSION pg_trgm;' | psql -U postgres
-    fi
+    echo 'CREATE EXTENSION unaccent;' | psql
+    echo 'CREATE EXTENSION pg_trgm;' | psql
     indico db prepare
 fi
 
