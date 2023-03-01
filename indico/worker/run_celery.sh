56a84de7269e8a69ddc97c4a4a87d6b5cd5a739a
Tomas Roun <tomas.roun8@gmail.com>
2023-03-01 15:39:31 +0100

    Make sure the host matches Indico's server name

--- indico/worker/run_celery.sh
+++ indico/worker/run_celery.sh
@@ -3,10 +3,12 @@
 /opt/indico/set_user.sh
 . /opt/indico/.venv/bin/activate
 
+# Wait for Indico to setup the DB
 echo 'Waiting for indico-web to be online...'
-while [[ "$(curl -L --max-time 10 -s -o /dev/null -w ''%{http_code}'' 'http://indico-web:59999')" != "200" ]]; do
+BASE_URL="${SERVICE_HOSTNAME}:${SERVICE_PORT}"
+while [[ $(curl -L --max-time 10 -s -o /dev/null --header "Host: ${BASE_URL}" -w ''%{http_code}'' 'http://indico-web:59999') != "200" ]]; do
     sleep 10;
-    echo 'Waiting...'
+    echo 'Waiting for indico-web to be online...'
 done
 
 echo 'Starting Celery...'
