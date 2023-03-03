f3b0f68cffa01b977511da477859a3c974c86bf7
Tomas Roun <tomas.roun8@gmail.com>
2023-03-03 15:24:25 +0100

    Ping DB directly to check if it's ready

--- indico/worker/run_celery.sh
+++ indico/worker/run_celery.sh
@@ -3,11 +3,16 @@
 /opt/indico/set_user.sh
 . /opt/indico/.venv/bin/activate
 
-# Wait for Indico to setup the DB
-echo 'Waiting for indico-web to be online...'
-while [[ $(curl -L --max-time 10 -s -o /dev/null --header "Host: localhost:9090" -w ''%{http_code}'' 'http://indico-web:59999') != "200" ]]; do
-    sleep 10;
-    echo 'Waiting for indico-web to be online...'
+check_db_ready() {
+    psql -c 'SELECT COUNT(*) FROM events.events'
+}
+
+# Wait until the DB becomes ready
+check_db_ready
+until [ $? -eq 0 ]; do
+    echo "Waiting for DB to be ready..."
+    sleep 10
+    check_db_ready
 done
 
 echo 'Starting Celery...'
